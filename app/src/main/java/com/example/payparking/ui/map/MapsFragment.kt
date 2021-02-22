6f25e51b52730a5296563857e863be43fe2ef87c
vankoowe <idgamov@gmail.com>
2021-02-22 13:11:09 +0200

    Added visualization of parking areas on the map and working hours of the area and not working heatMap.

--- app/src/main/java/com/example/payparking/ui/map/MapsFragment.kt
+++ app/src/main/java/com/example/payparking/ui/map/MapsFragment.kt
@@ -1,354 +1,137 @@
 package com.example.payparking.ui.map
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
-import android.os.Build
+import android.app.AlertDialog
+import android.graphics.Color
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.payparking.R
-import com.google.android.gms.location.*
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import androidx.annotation.RawRes
+import com.google.maps.android.heatmaps.Gradient
+import com.google.maps.android.heatmaps.HeatmapTileProvider
+import org.json.JSONArray
+import org.json.JSONException
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 @Suppress("DEPRECATED_IDENTITY_EQUALS")
 class MapsFragment : Fragment() {
 
-//    private val mMap: GoogleMap? = null
-//    private lateinit var currentLocation: Location
-//    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-//    private val permissionCode = 101
-
-//    private var mLocationRequest: LocationRequest? = null
-//    private val UPDATE_INTERVAL = (10 * 1000).toLong()  /* 10 secs */
-//    private val FASTEST_INTERVAL: Long = 2000 /* 2 sec */
-//
-//    private var latitude = 0.0
-//    private var longitude = 0.0
-
     private lateinit var mGoogleMap: GoogleMap
-
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-
-    // in onCreate() initialize FusedLocationProviderClient
-
-
-    // globally declare LocationRequest
-    private lateinit var locationRequest: LocationRequest
-
-    // globally declare LocationCallback
-    private lateinit var locationCallback: LocationCallback
-
-
-    /**
-     * call this method in onCreate
-     * onLocationResult call when location is changed
-     */
-    private fun getLocationUpdates()
-    {
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
-        locationRequest = LocationRequest()
-        locationRequest.interval = 50000
-        locationRequest.fastestInterval = 50000
-        locationRequest.smallestDisplacement = 170f // 170 m = 0.1 mile
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //set according to your app function
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                locationResult ?: return
-
-                if (locationResult.locations.isNotEmpty()) {
-                    // get latest location
-                    val location =
-                        locationResult.lastLocation
-                    // use your location object
-                    // get latitude , longitude and other info from this
-                }
-
-
-            }
-        }
-    }
-
-    //start location updates
-    private fun startLocationUpdates() {
-        if (ContextCompat.checkSelfPermission(requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION) !==
-            PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    requireActivity(),
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-            ) {
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
-                )
-            } else {
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
-                )
-            }
-        }
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            null /* Looper */
-        )
-    }
-
-    // stop location updates
-    private fun stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }
-
-    // stop receiving location update when activity not visible/foreground
-    override fun onPause() {
-        super.onPause()
-        stopLocationUpdates()
-    }
-
-    // start receiving location update when activity  visible/foreground
-    override fun onResume() {
-        super.onResume()
-        startLocationUpdates()
-    }
-
 
     companion object {
         fun newInstance() = MapsFragment()
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//        startLocationUpdates()
-//    }
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
-//        getLocationUpdates()
-//    }
-
-    override fun onStart() {
-        super.onStart()
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
-        getLocationUpdates()
-    }
-
-    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
 
-//        mGoogleMap = googleMap;
-//
-//        if (mGoogleMap != null) {
-//            mGoogleMap!!.addMarker(MarkerOptions().position(LatLng(latitude, longitude)).title("Current Location"))
-//        }
-//        val sydney = LatLng(-34.0, 151.0)
-//        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
-//        val markerOptions = MarkerOptions().position(latLng).title("I am here!")
+        var latLngs: List<LatLng?>? = null
 
-//        if (ContextCompat.checkSelfPermission(requireContext(),
-//                Manifest.permission.ACCESS_FINE_LOCATION) !==
-//            PackageManager.PERMISSION_GRANTED) {
-//            if (ActivityCompat.shouldShowRequestPermissionRationale(
-//                    requireActivity(),
-//                    Manifest.permission.ACCESS_FINE_LOCATION
-//                )
-//            ) {
-//                ActivityCompat.requestPermissions(
-//                    requireActivity(),
-//                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
-//                )
-//            } else {
-//                ActivityCompat.requestPermissions(
-//                    requireActivity(),
-//                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
-//                )
-//            }
-//        }
+        try {
+            latLngs = readItems()
+        } catch (e: JSONException) {
+            Toast.makeText(context, "Problem reading list of locations.", Toast.LENGTH_LONG)
+                .show()
+        }
 
-//        getLastKnownLocation(requireContext())
+        val colors = intArrayOf(
+            Color.rgb(102, 225, 0),  // green
+            Color.rgb(255, 0, 0) // red
+        )
+        val startPoints = floatArrayOf(0.2f, 1f)
+        val gradient = Gradient(colors, startPoints)
 
+        val provider = HeatmapTileProvider.Builder()
+            .data(latLngs)
+            .gradient(gradient)
+            .build()
 
+        val tileOverlay = googleMap?.addTileOverlay(
+            TileOverlayOptions()
+                .tileProvider(provider)
+        )
+        provider.setOpacity(0.7)
+        tileOverlay?.clearTileCache()
 
-        //fetchLocation()
-//        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-//        googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-//        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5f))
-//        googleMap?.addMarker(markerOptions)
+        val greenZone = googleMap.addPolygon(PolygonOptions()
+            .clickable(true)
+            .addAll(
+                readZones(R.raw.greenzones))
+            .strokeColor(Color.GREEN)
+            .fillColor(Color.GREEN))
 
+        val blueZone = googleMap.addPolygon(PolygonOptions()
+            .clickable(true)
+            .addAll(
+                readZones(R.raw.bluezones))
+            .strokeColor(Color.BLUE)
+            .fillColor(Color.BLUE))
 
+        googleMap.setOnPolygonClickListener(GoogleMap.OnPolygonClickListener {
+            if(it == blueZone) {
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("Синя зона")
+                builder.setMessage("Работно време:\n" +
+                        "Максимална продължителност на паркиране до 2 (два) часа;\n" +
+                        "Цена - 2 лева на час;\n" +
+                        "Работни дни в часовия диапазон от 08.30 до 19.30 часа;\n" +
+                        "Събота - в часовия диапазон от 10.00 до 18.00 часа.")
+                val mDialog = builder.create()
+                mDialog.show()
+            } else if(it == greenZone) {
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("Зелена зона")
+                builder.setMessage("Работно време:\n" +
+                        "Максимална продължителност на паркиране до 4 (четири) часа;\n" +
+                        "Цена - 1 лева на час\n" +
+                        "Работни дни в часовия диапазон от 08.30 до 19.30 часа;\n" +
+                        "Събота - в часовия диапазон от 10.00 до 18.00 часа.")
+                val mDialog = builder.create()
+                mDialog.show()
+            }
+
+        })
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(42.70, 23.33), 6f))
     }
 
-//    protected fun startLocationUpdates() {
-//        // initialize location request object
-//        mLocationRequest = LocationRequest.create()
-//        mLocationRequest!!.run {
-//            setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-//            setInterval(UPDATE_INTERVAL)
-//            setFastestInterval(FASTEST_INTERVAL)
-//        }
-//
-//        // initialize location setting request builder object
-//        val builder = LocationSettingsRequest.Builder()
-//        builder.addLocationRequest(mLocationRequest!!)
-//        val locationSettingsRequest = builder.build()
-//
-//        // initialize location service object
-//        val settingsClient = LocationServices.getSettingsClient(requireActivity())
-//        settingsClient!!.checkLocationSettings(locationSettingsRequest)
-//
-//        // call register location listener
-//        registerLocationListner()
-//    }
-//
-//    private fun registerLocationListner() {
-//        // initialize location callback object
-//        val locationCallback = object : LocationCallback() {
-//            override fun onLocationResult(locationResult: LocationResult?) {
-//                onLocationChanged(locationResult!!.getLastLocation())
-//            }
-//        }
-//        // 4. add permission if android version is greater then 23
-//        if(Build.VERSION.SDK_INT >= 23 && checkPermission()) {
-//            LocationServices.getFusedLocationProviderClient(requireActivity()).requestLocationUpdates(mLocationRequest, locationCallback, Looper.myLooper())
-//        }
-//    }
-//
-//    //
-//    private fun onLocationChanged(location: Location) {
-//        val location = LatLng(location.latitude, location.longitude)
-//        mGoogleMap!!.clear()
-//        mGoogleMap!!.addMarker(MarkerOptions().position(location).title("Current Location"))
-//        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
-//    }
-//
-//    private fun checkPermission() : Boolean {
-//        if (ContextCompat.checkSelfPermission(requireContext() , android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//            return true;
-//        } else {
-//            requestPermissions()
-//            return false
-//        }
-//    }
-//
-//    private fun requestPermissions() {
-//        ActivityCompat.requestPermissions(requireActivity(), arrayOf("Manifest.permission.ACCESS_FINE_LOCATION"),1)
-//    }
-//
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if(requestCode == 1) {
-//            if (permissions[0] == android.Manifest.permission.ACCESS_FINE_LOCATION ) {
-//                registerLocationListner()
-//            }
-//        }
-//    }
+    @Throws(JSONException::class)
+    private fun readItems(): List<LatLng?> {
+        val result: MutableList<LatLng?> = ArrayList()
 
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-//                                            grantResults: IntArray) {
-//        when (requestCode) {
-//            1 -> {
-//                if (grantResults.isNotEmpty() && grantResults[0] ==
-//                    PackageManager.PERMISSION_GRANTED) {
-//                    if ((ContextCompat.checkSelfPermission(requireContext(),
-//                            Manifest.permission.ACCESS_FINE_LOCATION) ===
-//                                PackageManager.PERMISSION_GRANTED)) {
-//                        Toast.makeText(activity, "Permission Granted", Toast.LENGTH_SHORT).show()
-//                    }
-//                } else {
-//                    Toast.makeText(activity, "Permission Denied", Toast.LENGTH_SHORT).show()
-//                }
-//                return
-//            }
-//        }
-//    }
+        result.add(LatLng(-35.016, 143.321))
+        result.add(LatLng(-34.747, 145.592))
+        result.add(LatLng(-34.364, 147.891))
+        result.add(LatLng(-33.501, 150.217))
+        result.add(LatLng(-32.306, 149.248))
+        result.add(LatLng(-32.491, 147.309))
+        return result
+    }
 
-//    private fun fetchLocation() {
-//        if (ContextCompat.checkSelfPermission(requireContext(),
-//                Manifest.permission.ACCESS_FINE_LOCATION) !==
-//            PackageManager.PERMISSION_GRANTED) {
-//            if (ActivityCompat.shouldShowRequestPermissionRationale(
-//                    requireActivity(),
-//                    Manifest.permission.ACCESS_FINE_LOCATION
-//                )
-//            ) {
-//                ActivityCompat.requestPermissions(
-//                    requireActivity(),
-//                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
-//                )
-//            } else {
-//                ActivityCompat.requestPermissions(
-//                    requireActivity(),
-//                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
-//                )
-//            }
-//        }
-//        val task = fusedLocationProviderClient.lastLocation
-//        task.addOnSuccessListener { location ->
-//            if (location != null) {
-//                currentLocation = location
-//            }
-//        }
-//    }
-//
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
-//                                            grantResults: IntArray) {
-//        when (requestCode) { permissionCode ->
-//            if (grantResults.isNotEmpty() && grantResults[0] ==
-//            PackageManager.PERMISSION_GRANTED) {
-//                fetchLocation()
-//            }
-//        }
-//    }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(requireActivity())
-//        fetchLocation()
-//    }
+    @Throws(JSONException::class)
+    private fun readZones(@RawRes resource: Int): MutableList<LatLng?> {
+        val result: MutableList<LatLng?> = ArrayList()
+        val inputStream = context?.resources!!.openRawResource(resource)
+        val json = Scanner(inputStream).useDelimiter("\\A").next()
+        val array = JSONArray(json)
+        for (i in 0 until array.length()) {
+            val `object` = array.getJSONObject(i)
+            val lat = `object`.getDouble("lat")
+            val lng = `object`.getDouble("lng")
+            result.add(LatLng(lat, lng))
+        }
 
-//    @SuppressLint("MissingPermission")
-//    private fun getLastKnownLocation(context: Context) {
-// val locationManager: LocationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//        val providers: List<String> = locationManager.getProviders(true)
-//        var location: Location? = null
-//        for (i in providers.size - 1 downTo 0) {
-//            locationManager.getLastKnownLocation(providers[i]).also { location = it }
-//            if (location != null)
-//                break
-//        }
-//        val gps = DoubleArray(2)
-//        if (location != null) {
-//            gps[0] = location!!.getLatitude()
-//            gps[1] = location!!.getLongitude()
-//            Log.e("gpsLat",gps[0].toString())
-//            Log.e("gpsLong",gps[1].toString())
-//
-//        }
-//
-//    }
+        return result
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
